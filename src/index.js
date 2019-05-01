b913bca0952d006f65ca9efc1fbca14b8250421a
Diego <diego.galan90@gmail.com>
2019-05-01 17:40:50 +0200

    my first commit

---
+++ src/index.js
@@ -0,0 +1,50 @@
+// Set up index.html to load the bundle
+// Make sure to load uuid via an npm module when necessary
+
+// --
+
+// Add necessary imports
+import { setFilters } from './filters'
+import { createTodo, loadTodos } from './todos'
+import { renderTodos } from './views'
+
+// Render initial todos
+renderTodos()
+
+// Set up search text handler
+document.querySelector('#search-text').addEventListener('input', (e) => {
+   setFilters({
+        searchText: e.target.value
+    })
+    renderTodos()
+})
+
+// Set up checkbox handler
+document.querySelector('#hide-completed').addEventListener('change', (e) => {
+    setFilters({
+        hideCompleted: e.target.checked
+    })
+    renderTodos()
+    console.log(getFilters())
+})
+
+// Set up form submission handler
+document.querySelector('#new-todo').addEventListener('submit', (e) => {
+    const text = e.target.elements.text.value.trim()
+    e.preventDefault()
+    
+    if (text.length > 0) {
+        createTodo(text)
+        renderTodos()
+        e.target.elements.text.value = ''
+    }
+    
+})
+
+// Bonus: Add a watcher for local storage
+window.addEventListener('storage', (e) => {
+    if (e.key === 'todos') {
+        loadTodos()
+        renderTodos()
+    }
+})
---
+++ src/views.js
@@ -0,0 +1,92 @@
+import { getFilters, setFilters } from './filters'
+import { toggleTodo, removeTodo, createTodo, getTodos, saveTodos, loadTodos } from './todos'
+
+// renderTodos
+// Arguments: none
+// Return value: none
+const renderTodos = () => {
+    const todoEl = document.querySelector('#todos')
+    const filters = getFilters()
+    const filteredTodos = getTodos().filter((todo) => {
+        const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+        const hideCompletedMatch = !filters.hideCompleted || !todo.completed
+
+        return searchTextMatch && hideCompletedMatch
+    })
+
+    const incompleteTodos = filteredTodos.filter((todo) => !todo.completed)
+
+    todoEl.innerHTML = ''
+    todoEl.appendChild(generateSummaryDOM(incompleteTodos))
+
+
+    if (filteredTodos.length > 0) {
+        filteredTodos.forEach((todo) => {
+            todoEl.appendChild(generateTodoDOM(todo))
+        })
+    } else {
+        const messageEl = document.createElement('p')
+        messageEl.textContent = 'No to-dos to show'
+        messageEl.classList.add('empty-message')
+        todoEl.appendChild(messageEl)
+    }
+}
+
+
+// generateTodoDOM
+// Arguments: todo
+// Return value: the todo element
+const generateTodoDOM = (todo) => {
+    const todos = getTodos()
+    const filters = getFilters()
+    
+    const todoEl = document.createElement('label')
+    const containerEl = document.createElement('div')
+    const checkbox = document.createElement('input')
+    const todoText = document.createElement('span')
+    const removeButton = document.createElement('button')
+
+    // Setup todo checkbox
+    checkbox.setAttribute('type', 'checkbox')
+    checkbox.checked = todo.completed
+    containerEl.appendChild(checkbox)
+    checkbox.addEventListener('change', () => {
+        toggleTodo(todo.id)
+        renderTodos()
+    })
+
+    // Setup todo text
+    todoText.textContent = todo.text
+    containerEl.appendChild(todoText)
+
+    // Setup container
+    todoEl.classList.add('list-item')
+    containerEl.classList.add('list-item__container')
+    todoEl.appendChild(containerEl)
+
+    // Setup remove button
+    removeButton.textContent = 'remove'
+    removeButton.classList.add('button', 'button--text')
+    todoEl.appendChild(removeButton)
+    removeButton.addEventListener('click', () => {
+        removeTodo(todo.id)
+        renderTodos()
+    })
+
+    return todoEl
+}
+
+// generateSummaryDOM
+// Arguments: incompletedTodos
+// Return value: the summary element
+const generateSummaryDOM = (incompleteTodos) => {
+    const summary = document.createElement('h2')
+    const plural = incompleteTodos.length === 1 ? '' : 's'
+    summary.classList.add('list-title')
+    summary.textContent = `You have ${incompleteTodos.length} todo${plural} left`
+
+    return summary
+}
+
+// Make sure to set up the exports
+export { generateSummaryDOM, generateTodoDOM, renderTodos}
